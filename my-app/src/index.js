0149e72ad7ffe643c217ac19fd9af0c9d56d0c26
laurenarthur <arthur_lauren@columbusstate.edu>
2021-02-08 21:45:26 +0000

    Working on dashboard

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,93 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './sb-admin-2.css';
 
 class App extends React.Component{
     render(){
         return(
-            <h1>Dashboard</h1>
+            <div>
+                <div id="wrapper">
+                    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+                        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                            <div class="sidebar-brand-icon rotate-n-15">
+                                <i class="fas fa-laugh-wink"></i>
+                            </div>
+                            <div class="sidebar-brand-text mx-3"><sub>Crisis Managment System</sub></div>
+                        </a>
+
+                        <hr class="sidebar-divider my-0"></hr>
+
+                        <div class="sidebar-heading">
+                            Interface
+                        </div>
+                        <li class="nav-item">
+                             <a class="nav-link" href="charts.html">
+                                <i class="fas fa-fw fa-chart-area"></i>
+                                <span>Home</span></a>
+                        </li>
+                         <li class="nav-item">
+                             <a class="nav-link" href="charts.html">
+                                <i class="fas fa-fw fa-chart-area"></i>
+                                <span>Explore</span></a>
+                        </li>
+                        <li class="nav-item">
+                             <a class="nav-link" href="charts.html">
+                                <i class="fas fa-fw fa-chart-area"></i>
+                                <span>Notifications</span></a>
+                        </li>
+                         <li class="nav-item">
+                             <a class="nav-link" href="charts.html">
+                                <i class="fas fa-fw fa-chart-area"></i>
+                                <span>Forum</span></a>
+                        </li>
+                        <li class="nav-item">
+                             <a class="nav-link" href="charts.html">
+                                <i class="fas fa-fw fa-chart-area"></i>
+                                <span>Profile</span></a>
+                        </li>
+
+                         <hr class="sidebar-divider d-none d-md-block"></hr>
+                         <div class="text-center d-none d-md-inline">
+                             <button class="rounded-circle border-0" id="sidebarToggle"></button>
+                        </div>
+
+                    </ul>
+
+                    <div id="content-wrapper" class="d-flex flex-column">
+                        <div id="content">
+                            <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                                <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                                    <i class="fa fa-bars"></i>
+                                </button>
+
+                                <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
+                                        aria-label="Search" aria-describedby="basic-addon2"></input>
+                                        <div class="input-group-append">
+                                            <button class="btn btn-primary" type="button">
+                                                <i class="fas fa-search fa-sm"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form>                                 
+                            </nav>
+
+                            <div class="container-fluid">
+                                <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                                    <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+                                </div>
+
+                                <div class="row">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
         )
     }
 
